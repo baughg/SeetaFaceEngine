f0057ce4905e101f600991a67bb5936dc065d9f6
Gary Baugh <baughg@tcd.ie>
2018-04-15 14:27:52 +0100

    hide

--- FaceDetection/src/test/facedetection_test.cpp
+++ FaceDetection/src/test/facedetection_test.cpp
@@ -1,33 +1,33 @@
 /*
- *
- * This file is part of the open-source SeetaFace engine, which includes three modules:
- * SeetaFace Detection, SeetaFace Alignment, and SeetaFace Identification.
- *
- * This file is an example of how to use SeetaFace engine for face detection, the
- * face detection method described in the following paper:
- *
- *
- *   Funnel-structured cascade for multi-view face detection with alignment awareness,
- *   Shuzhe Wu, Meina Kan, Zhenliang He, Shiguang Shan, Xilin Chen.
- *   In Neurocomputing (under review)
- *
- *
- * Copyright (C) 2016, Visual Information Processing and Learning (VIPL) group,
- * Institute of Computing Technology, Chinese Academy of Sciences, Beijing, China.
- *
- * The codes are mainly developed by Shuzhe Wu (a Ph.D supervised by Prof. Shiguang Shan)
- *
- * As an open-source face recognition engine: you can redistribute SeetaFace source codes
- * and/or modify it under the terms of the BSD 2-Clause License.
- *
- * You should have received a copy of the BSD 2-Clause License along with the software.
- * If not, see < https://opensource.org/licenses/BSD-2-Clause>.
- *
- * Contact Info: you can send an email to SeetaFace@vipl.ict.ac.cn for any problems.
- *
- * Note: the above information must be kept whenever or wherever the codes are used.
- *
- */
+*
+* This file is part of the open-source SeetaFace engine, which includes three modules:
+* SeetaFace Detection, SeetaFace Alignment, and SeetaFace Identification.
+*
+* This file is an example of how to use SeetaFace engine for face detection, the
+* face detection method described in the following paper:
+*
+*
+*   Funnel-structured cascade for multi-view face detection with alignment awareness,
+*   Shuzhe Wu, Meina Kan, Zhenliang He, Shiguang Shan, Xilin Chen.
+*   In Neurocomputing (under review)
+*
+*
+* Copyright (C) 2016, Visual Information Processing and Learning (VIPL) group,
+* Institute of Computing Technology, Chinese Academy of Sciences, Beijing, China.
+*
+* The codes are mainly developed by Shuzhe Wu (a Ph.D supervised by Prof. Shiguang Shan)
+*
+* As an open-source face recognition engine: you can redistribute SeetaFace source codes
+* and/or modify it under the terms of the BSD 2-Clause License.
+*
+* You should have received a copy of the BSD 2-Clause License along with the software.
+* If not, see < https://opensource.org/licenses/BSD-2-Clause>.
+*
+* Contact Info: you can send an email to SeetaFace@vipl.ict.ac.cn for any problems.
+*
+* Note: the above information must be kept whenever or wherever the codes are used.
+*
+*/
 
 #include <cstdint>
 #include <fstream>
@@ -41,71 +41,112 @@
 
 using namespace std;
 
+typedef struct facebox
+{
+	unsigned x;
+	unsigned y;
+	unsigned width;
+	unsigned height;
+	float score;
+}facebox;
+
 int main(int argc, char** argv) {
-  if (argc < 3) {
-      cout << "Usage: " << argv[0]
-          << " image_path model_path"
-          << endl;
-      return -1;
-  }
+	if (argc < 3) {
+		cout << "Usage: " << argv[0]
+			<< " image_path model_path"
+			<< endl;
+		return -1;
+	}
 
-  const char* img_path = argv[1];
-  seeta::FaceDetection detector(argv[2]);
+	const char* img_path = argv[1];
+	seeta::FaceDetection detector(argv[2]);
 
-  detector.SetMinFaceSize(40);
-  detector.SetScoreThresh(2.f);
-  detector.SetImagePyramidScaleFactor(0.8f);
-  detector.SetWindowStep(4, 4);
+	detector.SetMinFaceSize(40);
+	detector.SetScoreThresh(2.f);
+	detector.SetImagePyramidScaleFactor(0.8f);
+	detector.SetWindowStep(4, 4);
 
-  cv::Mat img = cv::imread(img_path, cv::IMREAD_UNCHANGED);
-  cv::Mat img_gray;
+	cv::Mat img = cv::imread(img_path, cv::IMREAD_UNCHANGED);
+	cv::Mat img_gray;
 
-  if (img.channels() != 1)
-    cv::cvtColor(img, img_gray, cv::COLOR_BGR2GRAY);
-  else
-    img_gray = img;
+	if (img.channels() != 1)
+		cv::cvtColor(img, img_gray, cv::COLOR_BGR2GRAY);
+	else
+		img_gray = img;
 
-  seeta::ImageData img_data;
-  img_data.data = img_gray.data;
-  img_data.width = img_gray.cols;
-  img_data.height = img_gray.rows;
-  img_data.num_channels = 1;
+	seeta::ImageData img_data;
+	img_data.data = img_gray.data;
+	img_data.width = img_gray.cols;
+	img_data.height = img_gray.rows;
+	img_data.num_channels = 1;
 
-  long t0 = cv::getTickCount();
-  std::vector<seeta::FaceInfo> faces = detector.Detect(img_data);
-  long t1 = cv::getTickCount();
-  double secs = (t1 - t0)/cv::getTickFrequency();
+	long t0 = cv::getTickCount();
+	std::vector<seeta::FaceInfo> faces = detector.Detect(img_data);
+	long t1 = cv::getTickCount();
+	double secs = (t1 - t0) / cv::getTickFrequency();
 
-  cout << "Detections takes " << secs << " seconds " << endl;
+	cout << "Detections takes " << secs << " seconds " << endl;
 #ifdef USE_OPENMP
-  cout << "OpenMP is used." << endl;
+	cout << "OpenMP is used." << endl;
 #else
-  cout << "OpenMP is not used. " << endl;
+	cout << "OpenMP is not used. " << endl;
 #endif
 
 #ifdef USE_SSE
-  cout << "SSE is used." << endl;
+	cout << "SSE is used." << endl;
 #else
-  cout << "SSE is not used." << endl;
+	cout << "SSE is not used." << endl;
 #endif
 
-  cout << "Image size (wxh): " << img_data.width << "x" 
-      << img_data.height << endl;
+	cout << "Image size (wxh): " << img_data.width << "x"
+		<< img_data.height << endl;
 
-  cv::Rect face_rect;
-  int32_t num_face = static_cast<int32_t>(faces.size());
+	cv::Rect face_rect;
+	int32_t num_face = static_cast<int32_t>(faces.size());
+	std::string detection_report_file = std::string(img_path);
 
-  for (int32_t i = 0; i < num_face; i++) {
-    face_rect.x = faces[i].bbox.x;
-    face_rect.y = faces[i].bbox.y;
-    face_rect.width = faces[i].bbox.width;
-    face_rect.height = faces[i].bbox.height;
 
-    cv::rectangle(img, face_rect, CV_RGB(0, 0, 255), 4, 8, 0);
-  }
+#ifdef WIN_SHOW
+	for (int32_t i = 0; i < num_face; i++) {
+		face_rect.x = faces[i].bbox.x;
+		face_rect.y = faces[i].bbox.y;
+		face_rect.width = faces[i].bbox.width;
+		face_rect.height = faces[i].bbox.height;
 
-  cv::namedWindow("Test", cv::WINDOW_AUTOSIZE);
-  cv::imshow("Test", img);
-  cv::waitKey(0);
-  cv::destroyAllWindows();
+		cv::rectangle(img, face_rect, CV_RGB(0, 0, 255), 4, 8, 0);
+	}
+
+	cv::namedWindow("Test", cv::WINDOW_KEEPRATIO);
+	cv::imshow("Test", img);
+	cv::waitKey(0);
+	cv::destroyAllWindows();
+#else
+	if (num_face)
+	{
+		facebox box;
+
+		detection_report_file.append(".face");
+		FILE* res_file = NULL;
+#ifdef _WIN32
+		fopen_s(&res_file, detection_report_file.c_str(), "wb");
+#else
+		res_file = fopen(detection_report_file.c_str(), "wb");
+#endif
+
+		if (res_file)
+		{
+			fwrite(&num_face, sizeof(int), 1, res_file);
+
+			for (int32_t i = 0; i < num_face; i++) {
+				box.x = faces[i].bbox.x;
+				box.y = faces[i].bbox.y;
+				box.width = faces[i].bbox.width;
+				box.height = faces[i].bbox.height;
+				box.score = faces[i].score;
+				fwrite(&box, sizeof(box), 1, res_file);
+			}
+			fclose(res_file);
+		}
+	}
+#endif
 }
